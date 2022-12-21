ad1e5bad3d6a9549e436170dbca89e174c61859c
sachinabs <sachinabs.js@gmail.com>
2022-12-21 16:41:28 +0530

    feat: dashboard page designing

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,12 +1,25 @@
 import './HomeStyles.css';
 import { Route, Routes, Link } from 'react-router-dom'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 export default function Home() {
 
     return (
-        <div className='animate__animated animate__fadeInUp'>
-            <h1>This is home page.</h1>
+        <div className='home-view animate__animated animate__fadeIn'>
+
+            <div className="view">
+                <div className="tab-title-container">
+                    <span className='tab-big-title'>Dashboard</span>
+                </div>
+                <div className="tab-container">
+                    <span className='tab-title'>Appointments</span>
+                    <span className='tab-title'>Patients  List</span>
+                </div>
+            </div>
+
+
             {/* <Link to={"/chat"}> go to chat</Link> */}
-            
+
         </div>
     )
 }
